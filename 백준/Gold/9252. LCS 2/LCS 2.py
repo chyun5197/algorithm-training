e7adbfdd643d1f5ec2fbbac727ff7f8e68afe54b
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-03-12 17:10:48 +0900

    [Gold IV] Title: LCS 2, Time: 1096 ms, Memory: 446352 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+a = input().rstrip()
+b = input().rstrip()
+
+dp = [[0] * (len(b)+1) for _ in range(len(a)+1)]
+words = [[''] * (len(b)+1) for _ in range(len(a)+1)]
+for i in range(1, len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]: #
+            dp[i][j] = dp[i-1][j-1] + 1
+            words[i][j] = words[i-1][j-1] + a[i-1]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            words[i][j] = words[i - 1][j] if dp[i-1][j] >= dp[i][j-1] else words[i][j-1]
+max = 0
+word = ''
+for i in range(1, len(a)+1):
+    for j in range(1, len(b)+1):
+        if dp[i][j]>max:
+            max = dp[i][j]
+            word = words[i][j]
+print(max)
+print(word)
